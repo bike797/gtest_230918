8de3f4345fccc14ddf3a8974f492cdc59bfda42f
Chansik Yun <chansigi@gmail.com>
2023-09-18 05:14:14 +0000

    .

--- 3_테스트픽스쳐4.cpp
+++ 3_테스트픽스쳐4.cpp
@@ -3,6 +3,9 @@
 
 class ImageProcessorTest : public testing::Test {
 public:
+    ImageProcessorTest() { std::cout << "ImageProcessorTest()" << std::endl; }
+    ~ImageProcessorTest() { std::cout << "~ImageProcessorTest()" << std::endl; }
+
     void SetUp() override
     {
         std::cout << "ImageProcessorTest.SetUp()" << std::endl;
@@ -23,3 +26,19 @@ TEST_F(ImageProcessorTest, BlurImage)
 {
     std::cout << "ImageProcessorTest.BlurImage()" << std::endl;
 }
+
+// 신선한 픽스쳐 전략
+// => 각 테스트케이스마다 새로운 객체를 생성하고 파괴하는 형태로 테스트케이스가 수행됩니다.
+/*
+ImageProcessorTest* ts = new ImageProcessorTest();
+ts->SetUp();
+ts->ResizeImage();
+ts->TearDown();
+delete ts;
+
+ImageProcessorTest* ts = new ImageProcessorTest();
+ts->SetUp();
+ts->BlurImage();
+ts->TearDown();
+delete ts;
+*/
