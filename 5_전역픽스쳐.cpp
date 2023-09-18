c7482eba02fa9f8fe85845f341c73f96fbb7ced9
Chansik Yun <chansigi@gmail.com>
2023-09-18 06:23:12 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -14,6 +14,8 @@ TEST(ImageProcessorTest, goo) { }
 // 1) testing::Environment 자식 클래스를 정의합니다.
 class MyEnvironment : public testing::Environment {
 public:
+    ~MyEnvironment() { std::cout << "~MyEnvironment()" << std::endl; }
+
     void SetUp() override
     {
         std::cout << "MyEnvironment.SetUp()" << std::endl;
@@ -25,13 +27,42 @@ public:
     }
 };
 
+class MyEnvironment2 : public testing::Environment {
+public:
+    ~MyEnvironment2() { std::cout << "~MyEnvironment()" << std::endl; }
+
+    void SetUp() override
+    {
+        std::cout << "MyEnvironment2.SetUp()" << std::endl;
+    }
+
+    void TearDown() override
+    {
+        std::cout << "MyEnvironment2.TearDown()" << std::endl;
+    }
+};
+
 // 2) 설치하는 방법 2가지
 //  - main을 통해 설치합니다.
+#if 1
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
+    testing::AddGlobalTestEnvironment(new MyEnvironment2);
     testing::AddGlobalTestEnvironment(new MyEnvironment);
+    // 주의: 반드시 new를 통해 생성된 객체를 등록해야 합니다.
 
     return RUN_ALL_TESTS();
 }
+#endif
+// - 전역 변수
+// : 두 개이상의 글로벌 픽스쳐를 등록할 경우,
+//   어떤 픽스쳐가 먼저 수행될지 보장할 수 없습니다.
+//  => main을 직접 정의해서 등록해야 합니다.
+#if 0
+testing::Environment* my_env = testing::AddGlobalTestEnvironment(new MyEnvironment);
+
+// 별도의 파일
+testing::Environment* my_env2 = testing::AddGlobalTestEnvironment(new MyEnvironment2);
+#endif
