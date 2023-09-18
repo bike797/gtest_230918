735acab7e54c25f5b6fb54c5da400793ccf31956
Chansik Yun <chansigi@gmail.com>
2023-09-18 04:03:21 +0000

    .

--- 3_테스트픽스쳐.cpp
+++ 3_테스트픽스쳐.cpp
@@ -28,3 +28,15 @@ TEST(CalcTest, PressPlus)
     // Assert
     ASSERT_EQ(calc->Display(), 20) << "10 더하기 10 하였을 때";
 }
+
+TEST(CalcTest, PressMinus)
+{
+    Calc* calc = new Calc;
+
+    calc->Enter(10);
+    calc->PressMinus();
+    calc->Enter(10);
+    calc->PressEquals();
+
+    ASSERT_EQ(calc->Display(), 0) << "10 빼기 10 하였을 때";
+}
