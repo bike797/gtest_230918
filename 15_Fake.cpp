19072b98e3b8d139083b4dd0c6f1aaf80072a807
Chansik Yun <chansigi@gmail.com>
2023-09-19 06:41:46 +0000

    .

--- 15_Fake.cpp
+++ 15_Fake.cpp
@@ -36,10 +36,51 @@ public:
     void Save(User* user)
     {
         database->SaveUser(user->GetName(), user);
+        // ...
     }
 
     User* Load(const std::string& name)
     {
+        // ...
         return database->LoadUser(name);
     }
 };
+
+#include <gtest/gtest.h>
+#include <map>
+
+// Fake Object Pattern
+// 의도: 아직 준비되지 않은 협력 객체로 인해서, 테스트 되지 않은 요구사항이 존재합니다.
+// 방법: 동일한 기능을 제공하는 가벼운 테스트 대역을 통해, 테스트 되지 않은 요구사항을 검증합니다.
+//   1) 협력 객체가 준비되지 않았을 때
+//   2) 협력 객체를 사용하기 어려울 때
+//   3) 협력 객체가 너무 느려서, 느린 테스트의 문제가 발생할 때
+
+class FakeDatebase : public IDatabase {
+    std::map<std::string, User*> data;
+
+public:
+    void SaveUser(const std::string& name, User* user) override
+    {
+        data[name] = user;
+    }
+
+    User* LoadUser(const std::string& name) override
+    {
+        return data[name];
+    }
+};
+
+TEST(RespositoryTest, Save)
+{
+    FakeDatebase fake;
+    Respository repo { &fake };
+    std::string testName = "test_name";
+    int testAge = 42;
+    User expected { testName, testAge };
+
+    repo.Save(&expected);
+    User* actual = repo.Load(testName);
+
+    EXPECT_EQ(expected, *actual);
+}
