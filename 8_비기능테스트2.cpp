933a59c3d9a36bd55ebc5bf9d38a2e9c758694e9
Chansik Yun <chansigi@gmail.com>
2023-09-19 00:40:21 +0000

    .

--- 8_비기능테스트2.cpp
+++ 8_비기능테스트2.cpp
@@ -41,12 +41,13 @@ bool DrawImage(const std::string& url)
 
     //...
 
-    delete p;
+    // delete p;
     return true;
 }
 
 #include <gtest/gtest.h>
 
+#if 0
 TEST(ImageTest, DrawImage)
 {
     int allocCount = Image::allocCount;
@@ -56,4 +57,36 @@ TEST(ImageTest, DrawImage)
 
     int diff = Image::allocCount - allocCount;
     EXPECT_EQ(diff, 0) << diff << " object(s) leaks";
+}
+#endif
+
+// 장점
+// 1) 기능적인 테스트와 비기능적인 테스트를 분리할 수 있습니다.
+// 2) 모든 테스트케이스는 메모리 검증을 수행합니다.
+class ImageTest : public testing::Test {
+protected:
+    int allocCount = 0;
+
+    void SetUp() override
+    {
+        allocCount = Image::allocCount;
+    }
+
+    void TearDown() override
+    {
+        int diff = Image::allocCount - allocCount;
+        EXPECT_EQ(diff, 0) << diff << " object(s) leaks";
+    }
+};
+
+TEST_F(ImageTest, DrawImage)
+{
+    bool result = DrawImage("https://a.com/a.png");
+    EXPECT_TRUE(result);
+}
+
+TEST_F(ImageTest, DrawImage2)
+{
+    bool result = DrawImage("https://a.com/a.png");
+    EXPECT_TRUE(result);
 }
