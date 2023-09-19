63d2b27e69cbbb18fd11f549cb4329f917b335d8
Chansik Yun <chansigi@gmail.com>
2023-09-19 00:35:26 +0000

    .

--- 8_비기능테스트2.cpp
+++ 8_비기능테스트2.cpp
@@ -15,8 +15,25 @@ public:
     {
         std::cout << "Draw Image: " << url << std::endl;
     }
+
+    static int allocCount;
+    void* operator new(size_t size)
+    {
+        ++allocCount;
+        return malloc(size);
+    }
+
+    void operator delete(void* p, size_t)
+    {
+        --allocCount;
+        free(p);
+    }
 };
 
+int Image::allocCount = 0;
+
+// SUT에 대해서 메모리 누수가 발생하는지 여부를 검증하고 싶습니다.
+//  1) SUT의 클래스의 메모리 할당/해지 연산을 재정의합니다.
 bool DrawImage(const std::string& url)
 {
     Image* p = new Image(url);
@@ -24,6 +41,7 @@ bool DrawImage(const std::string& url)
 
     //...
 
+    delete p;
     return true;
 }
 
@@ -31,6 +49,11 @@ bool DrawImage(const std::string& url)
 
 TEST(ImageTest, DrawImage)
 {
+    int allocCount = Image::allocCount;
+
     bool result = DrawImage("https://a.com/a.png");
     EXPECT_TRUE(result);
+
+    int diff = Image::allocCount - allocCount;
+    EXPECT_EQ(diff, 0) << diff << " object(s) leaks";
 }
