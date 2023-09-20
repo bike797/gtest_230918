2911cc0f2db2a70b8bfa58bbb1a95b712adfe534
Chansik Yun <chansigi@gmail.com>
2023-09-20 00:50:16 +0000

    .

--- 18_Mocking3.cpp
+++ 18_Mocking3.cpp
@@ -6,11 +6,11 @@ class StackInterface {
 public:
     virtual ~StackInterface() { }
 
-    virtual int GetSize() const = 0;
+    virtual int GetSize() const { return 0; }
     virtual void Push(const T& data) = 0;
 };
 
-// 템플릿 기반의 인터페이스도 Mocking 할수 있습니다.
+// 템플릿 기반의 인터페이스/추상클래스/부모클래스도 Mocking 할수 있습니다.
 template <typename T>
 class MockStackInterface : public StackInterface<T> {
 public:
