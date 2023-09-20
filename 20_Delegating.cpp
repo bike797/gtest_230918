ebd8d255d8b5a4bc82a1cbac45a7aee8f462ce7e
Chansik Yun <chansigi@gmail.com>
2023-09-20 02:32:04 +0000

    .

--- 20_Delegating.cpp
+++ 20_Delegating.cpp
@@ -26,6 +26,7 @@ void Process(Calc* p)
 // 행위 기반 검증도 수행할 수 있지만, 결과를 제어하는 기능을 제공하고 있습니다.
 // => Delegating(위임)
 // : ON_CALL
+//   - Stub, Fake를 Google Mock을 통해서 만들 수 있습니다.
 
 class MockCalc : public Calc {
 public:
@@ -35,12 +36,23 @@ public:
 
 using testing::Return;
 
+int add(int a, int b) { return a + b; }
+int sub(int a, int b) { return a - b; }
+
+struct Adder {
+    int operator()(int a, int b) const { return a + b; }
+};
+
 TEST(CalcTest, Process)
 {
     // Arrange
     MockCalc mock;
-    ON_CALL(mock, Add(10, 20)).WillByDefault(Return(30));
-    ON_CALL(mock, Sub(100, 50)).WillByDefault(Return(50));
+    // ON_CALL(mock, Add(10, 20)).WillByDefault(Return(30));
+    // ON_CALL(mock, Sub(100, 50)).WillByDefault(Return(50));
+    // ON_CALL(mock, Add).WillByDefault(&add); // 함수
+    // ON_CALL(mock, Sub).WillByDefault(&sub); // 함수
+    ON_CALL(mock, Add).WillByDefault(Adder {}); // 함수 객체
+    ON_CALL(mock, Sub).WillByDefault([](int a, int b) { return a - b; }); // 람다 표현식
 
     // Assert
     EXPECT_CALL(mock, Add(10, 20));
