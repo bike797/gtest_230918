721d9cace5ea3e207138b00646c30e1cafa9f833
Chansik Yun <chansigi@gmail.com>
2023-09-20 02:16:40 +0000

    .

--- 19_Mock종류.cpp
+++ 19_Mock종류.cpp
@@ -36,6 +36,10 @@ Uninteresting mock function call - returning default value.
 // 2) Nice Mock
 // => 경고도 발생하지 않습니다.
 
+// 3) Strict Mock
+// => 테스트가 실패합니다.
+// => 테스트의 통과 기준이 높아지고, 테스트 비용의 증가가 발생할 수 있습니다.
+
 class MockCar : public Car {
 public:
     MOCK_METHOD(void, Go, (), (override));
@@ -43,11 +47,13 @@ public:
 };
 
 using testing::NiceMock;
+using testing::StrictMock;
 
 TEST(CarTest, Process)
 {
     // MockCar mock;
-    NiceMock<MockCar> mock;
+    // NiceMock<MockCar> mock;
+    StrictMock<MockCar> mock;
 
     EXPECT_CALL(mock, Go);
     // EXPECT_CALL(mock, GetName);
