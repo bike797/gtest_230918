cecd9244e04b9b084e7069c9b9975f937c569507
Chansik Yun <chansigi@gmail.com>
2023-09-20 00:30:02 +0000

    .

--- 18_Mocking.cpp
+++ 18_Mocking.cpp
@@ -8,6 +8,14 @@ public:
 
     virtual void Play() = 0;
     virtual bool Stop(int n) = 0;
+
+    virtual std::string GetTitle() const = 0;
+    virtual void Go() const noexcept = 0;
+
+    virtual std::pair<bool, int> GetPair() const = 0;
+
+    // 아래 함수를 Mocking을 직접 해보세요.
+    virtual bool CheckMap(std::map<int, double> a, bool b) const = 0;
 };
 
 // Google Mock은 테스트 대역이기 때문에,
@@ -16,11 +24,23 @@ class MockMP3 : public MP3 {
 public:
     // MOCK_METHOD(반환타입, 메소드이름, (인자 정보), (한정자 정보));
 
+    // 주의 사항: 템플릿의 타입 인자가 2개 이상 존재하는 경우,
+    //          괄호로 감싸 주어야 합니다.
+
+    // std::pair<bool, int> GetPair() const override
+    MOCK_METHOD((std::pair<bool, int>), GetPair, (), (const, override));
+
     // void Play() override
     MOCK_METHOD(void, Play, (), (override));
 
     // bool Stop(int n) override
     MOCK_METHOD(bool, Stop, (int n), (override));
+
+    // std::string GetTitle() const override
+    MOCK_METHOD(std::string, GetTitle, (), (const, override));
+
+    // void Go() const noexcept override
+    MOCK_METHOD(void, Go, (), (const, noexcept, override));
 };
 
 TEST(MP3Test, Sample)
